fe720948532a93ae543dc3de5adebb28fa25bb14
Nitesh Kumar <103721139+Nitesh-kumar27@users.noreply.github.com>
2025-06-27 20:48:04 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-
+const authMiddleware = require("./middleware/auth.middleware");
 const healthRoutes = require("./routes/healthRoutes");
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
@@ -16,7 +16,7 @@ connectDB();
 app.use("/", healthRoutes);
 app.use("/auth", authRoutes);
 
-app.use("/user", userRoutes);
+app.use("/user", authMiddleware, userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
